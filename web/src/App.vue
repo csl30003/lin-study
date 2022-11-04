cede1392b8a48867d30e1601601b34c89e45edf6
离柒L <863577413@qq.com>
2022-11-04 10:54:43 +0800

    完成首页

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,22 +1,3 @@
 <template>
-  <el-container height="100%">
-    <el-header class="el-header" height="100px">
-      <h1 class="h1">LinStudy</h1>
-    </el-header>
-    <router-view></router-view>
-  </el-container>
+    <router-view style="height: 100%; width: 100%"></router-view>
 </template>
-
-<style scoped>
-.el-header{
-  display: table-cell;
-  vertical-align: middle;
-  background-color: #ffe05d;
-}
-.h1 {
-  text-align: center;
-  font-size: 60px;
-  color: white;
-  transform: translateY(-50%);
-}
-</style>
--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <el-main>
+    <el-main class="el-main">
+      <el-image :src="require('@/assets/back.png')"
+                style="margin: 0;position:absolute; height: 100%; width: 100%"></el-image>
+      <el-row>
+        <el-col align="center">
+          <h1 class="h1">LinStudy</h1>
+        </el-col>
+      </el-row>
       <el-row class="el-row">
         <el-col align="center">
           <div class="radius">
             <el-form :model="form" label-width="auto" size="large">
-              <el-form-item label="用户名">
-                <el-input v-model="form.name"/>
+              <br>
+              <br>
+              <el-form-item style="display: inline-block; width: 80%">
+                <el-input class="el-input" maxlength="30" show-word-limit placeholder="用户名" v-model="form.name"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input v-model="form.password" type="password"/>
+              <el-form-item style="display: inline-block; width: 80%">
+                <el-input class="el-input" maxlength="30" show-word-limit placeholder="密码" show-password
+                          v-model="form.password" type="password"/>
               </el-form-item>
 
               <el-row>
@@ -48,18 +58,39 @@ const form = reactive({
 </script>
 
 <style scoped>
+.h1 {
+  text-align: center;
+  font-size: 100px;
+  transform: translateY(-50%);
+  color: #ffbc37;
+  font-family: STLiti;
+}
+
 .radius {
   height: 70%;
-  width: 50%;
+  width: 400px;
   border: 1px solid var(--el-border-color);
   border-radius: 20px;
   margin-top: 100px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.5);
+  text-align:center;
+}
+
+.el-main {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
 }
 
 .el-row {
   margin-top: 10px;
   margin-bottom: 10px;
+}
+
+.el-input {
+  text-align: center;
+  vertical-align: middle;
 }
 
 .button-class {
