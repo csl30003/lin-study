6fa1d39ec908bbd8a599f996a6297a4278d5b44b
离柒L <863577413@qq.com>
2022-11-29 12:24:51 +0800

    修改mysql配置

--- server/database/mysql.go
+++ server/database/mysql.go
@@ -17,7 +17,8 @@ type Mysql struct {
 	user     string
 	password string
 	database string
-	db       *gorm.DB
+
+	db *gorm.DB
 }
 
 //
@@ -45,9 +46,28 @@ func (m *Mysql) Conn() {
 	m.database = conf.Cfg.Section("MYSQL").Key("database").String()
 
 	//  连接MySQL
-	dsn := m.user + ":" + m.password + "@tcp(" + m.host + ":" + m.port + ")/" + m.database + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := m.user + ":" + m.password + "@tcp(" + m.host + ":" + m.port + ")/" + m.database + "?charset=utf8mb4&parseTime=True&loc=UTC"
 	m.db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("MySQL初始化失败", err)
 	}
+
+	sqlDB, err := m.db.DB()
+	if err != nil {
+		log.Fatal("MySQL配置初始化失败", err)
+	}
+
+	maxOpenConn, err := conf.Cfg.Section("DATASOURCE").Key("max_open_conn").Int()
+	if err != nil {
+		log.Fatal("DataSource配置文件类型转换失败失败", err)
+	}
+	maxIdleConn, err := conf.Cfg.Section("DATASOURCE").Key("max_idle_conn").Int()
+	if err != nil {
+		log.Fatal("DataSource配置文件类型转换失败失败", err)
+	}
+
+	//  设置最大连接数
+	sqlDB.SetMaxOpenConns(maxOpenConn)
+	//  设置最大空闲连接数
+	sqlDB.SetMaxIdleConns(maxIdleConn)
 }
